7a25ff6be04adb828e659949b492c71a2b60c3a2
trmaxa2820 <60822358+trmaxa2820@users.noreply.github.com>
2020-10-08 00:05:50 +0300

    v0.1
    New Alram project create

---
+++ Alarm/Assets/Scripts/Alarm.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _volumeChangeValue;
+
+    private Coroutine _volumeChangerCoroutine;
+    private float _volume;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Player>(out Player player))
+        {
+            if (_audioSource.isPlaying)
+            {
+                StopCoroutine(_volumeChangerCoroutine);
+                _volumeChangerCoroutine = StartCoroutine(ChangeVolume(-_volumeChangeValue));
+            }
+            else
+            {
+                _audioSource.Play();
+                _volumeChangerCoroutine = StartCoroutine(ChangeVolume(_volumeChangeValue));
+            }
+        }
+    }
+
+    private IEnumerator ChangeVolume(float changeValue)
+    {
+        if(changeValue > 0)
+        {
+            while (_volume < 1)
+            {
+                _volume += changeValue * Time.deltaTime;
+                _audioSource.volume = _volume;
+                yield return null;
+            }
+        }
+        else
+        {
+            while(_volume > 0)
+            {
+                _volume += changeValue * Time.deltaTime;
+                _audioSource.volume = _volume;
+                yield return null;
+            }
+            _audioSource.Stop();
+            StopCoroutine(_volumeChangerCoroutine);
+        }
+    } 
+}
---
+++ Alarm/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * _movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.left * Time.deltaTime * _movementSpeed);
+        }
+    }
+}
